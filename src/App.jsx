8ea1a79c75f63ba0f1749ac6f851d44c0675d5ca
pere busquet <36750713+buski87@users.noreply.github.com>
2025-03-22 00:13:29 +0100

    initial config
    Configuracion inicial de todos los elementos para la web y la app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="font-sans bg-gray-100 text-gray-900">
+      <span>
+        <h1>Hello World!!!!</h1>
+      </span>
+    </div>
+  );
+}
+
+export default App;
